10259183f38909a08c10ab42cbb085dc69563272
fedepujol <fpujol@protonmail.com>
2021-11-12 17:37:25 -0300

    Use shipwright to export themes to vim

--- export.lua
+++ export.lua
@@ -1,4 +1,15 @@
-local lush = require('lush')
-local theme = require('lush_theme.cosmos')
+local theme = require('lush_theme.vcdark')
+local lushwright = require('shipwright.transform.lush')
 
-lush.export_to_buffer(theme)
+-- Call run and give the colorscheme theme
+---@diagnostic disable = undefined-global
+run(theme,
+	-- convert to a list of vimscript commands
+	lushwright.to_vimscript,
+	-- pass it through a vim-compatible script (removes blend)
+	lushwright.vim_compatible_vimscript,
+	-- Add a few housekeeping lines to make it work
+	{prepend, {"set background=dark"}},
+	-- write the theme to a file
+	{overwrite, "colors/vcdark.vim"}
+)
