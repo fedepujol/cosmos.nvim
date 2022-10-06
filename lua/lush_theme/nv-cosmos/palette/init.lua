f60762b6034c570e543db8c71512e0de55f9ca2c
fedepujol <9yjbd325@anonaddy.me>
2022-10-06 18:36:09 -0300

    upd: Cosmos palette

--- lua/lush_theme/nv-cosmos/palette/init.lua
+++ lua/lush_theme/nv-cosmos/palette/init.lua
@@ -2,37 +2,40 @@ local hsl = require('lush.hsl')
 
 ---@enum
 local palette = {
-	amber_800 = hsl('#FF8F00'),
-	bgray_900 = hsl('#263238'),
-	blue_200 = hsl('#90CAF9'),
-	blue_700 = hsl('#1976D2'),
-	cyan_200 = hsl('#80DEEA'),
+	lblue_400 = hsl('#29B6F6'),
+	blue_300 = hsl('#64B6F6'),
+	teal_300 = hsl('#4DB6AC'),
 	cyan_400 = hsl('#26C6DA'),
-	cyan_800 = hsl('#00838F'),
-	dorange_300 = hsl('#FF8A65'),
-	dorange_500 = hsl('#FF5722'),
-	dorange_800 = hsl('#D84315'),
-	dpurple_200 = hsl('#B39DDB'),
+	blue_700 = hsl('#1976D2'),
+	blue_200 = hsl('#90CAF9'),
+
+	orange_200 = hsl('#FFCC80'),
+	orange_400 = hsl('#FFA726'),
+	orange_700 = hsl('#F57C00'),
+
 	gray_300 = hsl('#E0E0E0'),
 	gray_400 = hsl('#B0B0B0'),
 	gray_700 = hsl('#616161'),
 	gray_800 = hsl('#424242'),
+	bgray_900 = hsl('#263238'),
 	gray_900 = hsl('#212121'),
+
+	lgreen_500 = hsl('#8BC34A'),
+	green_200 = hsl('#66BB6A'),
 	green_800 = hsl('#2E7D32'),
-	green_a700 = hsl('#00C853'),
-	indigo_300 = hsl('#7986CB'),
-	indigo_400 = hsl('#5C6BC0'),
-	lblue_400 = hsl('#29B6F6'),
-	lblue_700 = hsl('#0288D1'),
-	lime_500 = hsl('#CDDC39'),
-	orange_200 = hsl('#FFCC80'),
-	pink_100 = hsl('#F8BBD0'),
+
+	purple_200 = hsl('#CE93D8'),
 	purple_300 = hsl('#BA68C8'),
-	red_900 = hsl('#B71C1C'),
-	teal_300 = hsl('#4DB6AC'),
-	teal_600 = hsl('#00897B'),
+	purple_400 = hsl('#AB47BC'),
+
+	red_500 = hsl('#F44336'),
+	red_700 = hsl('#D32F3F'),
+
+	yellow_600 = hsl('#FDD835'),
 	yellow_700 = hsl('#FBC02D'),
-	yellow_a100 = hsl('#FFFF8D'),
+	yellow_800 = hsl('#F9A825'),
+
+	pink_300 = hsl('#F06292')
 }
 
 return palette
