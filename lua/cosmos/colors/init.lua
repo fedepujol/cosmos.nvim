9e740861b4a388d664c9e0b865f3668cb7d0ecda
fedepujol <fpujol@proton.me>
2024-05-27 18:11:46 -0300

    fix(colors): Rename colors

--- lua/cosmos/colors/init.lua
+++ lua/cosmos/colors/init.lua
@@ -12,41 +12,37 @@ return {
 	-- Foregrounds
 	foreground01 = hsl('#DFDFE0'),
 	foreground02 = hsl('#B0BEC5'),
-	foreground03 = hsl('#BDC3C2'),
 
 	-- General
-	boolean = hsl('#7EBA71'),
+	boolean = hsl('#A6DBFF'),
 	character = hsl('#F6955B'),
 	comment = hsl('#7C8083'),
 	comment_special = hsl('#D38AEA'),
-	constant = hsl('#67B0E8'),
+	constant = hsl('#71BAF2'),
+	delimiter = hsl('#BDC3C2'),
 	error = hsl('#E82424'),
-	funct = hsl('#4DB5BD'),
-	number = hsl('#71BAF2'),
-	parameter = hsl('#ABCF76'),
+	funct = hsl('#2DF4C0'),
+	parameter = hsl('#79925F'),
 	preproc = hsl('#82AAFF'),
+	special = hsl('#ABCF76'),
 	statement = hsl('#B4A4F4'),
 	statusline = hsl('#1E2326'),
 	string = hsl('#F9E2AF'),
 	type = hsl('#9D79D6'),
-	variable = hsl('#A6DBFF'),
-
-	-- tags -- niidea
-	-- debug -- rosa
+	float_border = hsl('#D67AD2'),
 
 	yellow04 = hsl('#FFCC00'),
-
 	green00 = hsl('#AFD700'),
-	green03 = hsl('#79925F'),
+	green01 = hsl('#7EBA71'),
 
-	blue00 = hsl('#2DF4C0'),
+	blue00 = hsl('#4DB5BD'),
+	blue01 = hsl('#67B0E8'),
 	blue02 = hsl('#223249'),
 	blue05 = hsl('#2D4F67'),
 	blue06 = hsl('#1C4474'),
 	blue10 = hsl('#49698F'),
 
 	pink02 = hsl('#ECB2F0'),
-	pink04 = hsl('#D67AD2'),
 
 	-- Diff
 	diff_add = hsl('#6BC568'),
